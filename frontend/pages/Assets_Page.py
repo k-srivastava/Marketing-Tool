35a198b50dbb0dd293a937777aff80140594e769
k-srivastava <srivastavak1223@gmail.com>
2025-12-08 23:07:01 +0530

    Added boilerplate for Streamlit UI.

---
+++ frontend/pages/Assets_Page.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+st.set_page_config(page_title='Asset Manager', initial_sidebar_state='collapsed')
+
+st.markdown(
+    """<h1 style="text-align: center;">Asset Manager</h1>
+    <p style='text-align: center; color: #808080;'>Upload your product details and visuals to get started.</p>""",
+    unsafe_allow_html=True
+)
+
+st.divider()
+
+with st.container():
+    st.subheader('Product Details')
+
+    product_description_raw = st.text_area(
+        'Product Description',
+        height=150,
+        placeholder='Your detailed product description goes here...',
+        help="Provide as much detail as possible for better results."
+    )
+
+st.divider()
+
+st.subheader('Product Assets')
+left, middle, right = st.columns(3, gap='large', vertical_alignment='top')
+
+with left:
+    st.markdown('##### Hero Image')
+    st.caption('Main representation of your product.')
+
+    hero_image_raw = st.file_uploader(
+        'Product Hero Image', type=['png', 'jpg'], accept_multiple_files=False, label_visibility='collapsed'
+    )
+
+with middle:
+    st.markdown('##### Brand Logo')
+    st.caption('Your brand logo (transparent background is recommended).')
+
+    logo_image_raw = st.file_uploader(
+        'Brand Hero', type=['png', 'jpg'], accept_multiple_files=False, label_visibility='collapsed'
+    )
+
+with right:
+    st.markdown('##### Support Images')
+    st.caption('Additional angles or context shots to be optionally included.')
+
+    support_images_raw = st.file_uploader('Support Images', type=['png', 'jpg'], label_visibility='collapsed')
+
+st.divider()
+
+_, middle, _ = st.columns([1, 2, 1])
+with middle:
+    st.button('Start Generation', type='primary', use_container_width=True)
