da04d3a73172daf8c3933790f640a925d349ceb8
rav666 <harshitagaurav2609@gmail.com>
2025-12-10 15:50:27 +0530

    Updated layout generation utilities.

--- frontend/middleware/layouts.py
+++ frontend/middleware/layouts.py
@@ -1,6 +1,7 @@
 """
 Utility functions for the layout preference page.
 """
+from PIL import Image, ImageOps
 
 
 def generate_page_labels(num_support_images: int) -> list[str]:
@@ -37,3 +38,78 @@ def generate_page_headings(num_support_images: int) -> list[str]:
         page_headings.append(f'Support Image ({i + 1}) Layout')
 
     return page_headings
+
+
+def create_option_thumbnails(
+        asset: Image.Image, canvas_size: int = 200, canvas_color: str = '#E5E5E5', border_color: str = '#14213D'
+) -> list[Image.Image]:
+    """
+    Generates a list of thumbnails for an image asset, each positioned differently on a canvas of specified size and
+    color, with an optional border around each canvas. The nine thumbnails represent all possible alignments of the
+    asset on the canvas: top-left, top-center, top-right, center-left, center, center-right, bottom-left,
+    bottom-center, and bottom-right.
+
+    :param asset: Image asset to be used for creating thumbnails.
+    :type asset: Image.Image
+    :param canvas_size: Size of the square canvas in pixels.
+    :type canvas_size: int
+    :param canvas_color: Background color of the canvas in a web-compatible color format.
+    :type canvas_color: str
+    :param border_color: Color of the border being added around each canvas in a web-compatible color format.
+    :type border_color: str
+
+    :return: List of thumbnail images containing the asset positioned on a canvas with the specified size and colors.
+    :rtype: list[Image.Image]
+    """
+    positions: list[tuple[int, int]] = [
+        (0, 0),
+        ((canvas_size - asset.width) // 2, 0),
+        (canvas_size - asset.width, 0),
+
+        (0, (canvas_size - asset.height) // 2),
+        ((canvas_size - asset.width) // 2, (canvas_size - asset.height) // 2),
+        (canvas_size - asset.width, (canvas_size - asset.height) // 2),
+
+        (0, canvas_size - asset.height),
+        ((canvas_size - asset.width) // 2, canvas_size - asset.height),
+        (canvas_size - asset.width, canvas_size - asset.height)
+    ]
+
+    thumbnails: list[Image.Image] = []
+    for position in positions:
+        canvas = Image.new('RGBA', (canvas_size, canvas_size), canvas_color)
+        canvas.paste(asset, position, asset)
+        bordered_canvas = ImageOps.expand(canvas, border=1, fill=border_color)
+        thumbnails.append(bordered_canvas)
+
+    return thumbnails
+
+
+def get_thumbnail_position_name(idx: int) -> str:
+    """
+    Get the corresponding position name for a thumbnail index.
+
+    :param idx: Thumbnail index.
+    :type idx: int
+
+    :return: Position name.
+    :rtype: str
+    """
+    if idx == 0:
+        return 'Top Left'
+    elif idx == 1:
+        return 'Top Center'
+    elif idx == 2:
+        return 'Top Right'
+    elif idx == 3:
+        return 'Middle Left'
+    elif idx == 4:
+        return 'Middle Center'
+    elif idx == 5:
+        return 'Middle Right'
+    elif idx == 6:
+        return 'Bottom Left'
+    elif idx == 7:
+        return 'Bottom Center'
+    else:
+        return 'Bottom Right'
