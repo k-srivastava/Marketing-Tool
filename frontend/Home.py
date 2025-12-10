f83ec7a82dd1b885c7571cc698a6c13d0d3c819d
k-srivastava <srivastavak1223@gmail.com>
2025-12-10 22:39:20 +0530

    Refined styling.

--- frontend/Home.py
+++ frontend/Home.py
@@ -1,62 +1,61 @@
 import streamlit as st
 
-st.set_page_config(page_title="Creative Studio", layout="wide")
+st.set_page_config(page_title='Creative Studio', layout='wide', initial_sidebar_state='collapsed')
 
 st.markdown("""
             <style>
 
-                body, html, [class*="css"] {
+                h1, h2, h3, h4, h5, p, div, label, span {
                     font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
                 }
 
                 /* Main gradient background */
                 .hero-section {
+                    margin-bottom: 50px;
+                    padding: 60px 20px 80px 20px;
+                    border-bottom-right-radius: 80px;
+                    border-bottom-left-radius: 80px;
                     background: linear-gradient(135deg, #4e09bd, #081f52);
-                    border-bottom-left-radius: 150px;
-                    border-bottom-right-radius: 150px;
-                    padding: 80px 40px;
+                    box-shadow: 0 10px 30px rgba(20, 33, 61, 0.15);
                 }
 
                 .hero-title-top {
                     font-size: 42px;
                     font-weight: 700;
-                    color: #041942;
                     margin-bottom: 30px;
-                    letter-spacing: 1px;
                     text-align: center;
-                    text
+                    letter-spacing: 1px;
+                    color: #14213D;
                 }
 
                 /* Centered Split Animated Title */
                 .hero-title-wrapper {
-                    width: 100%;
-                    display: flex;
-                    justify-content: center;
-                    margin-top: 40px;
-                }
-
-                .split-text-container {
                     display: flex;
                     align-items: center;
                     justify-content: center;
-                    font-size: 4rem;
-                    text-shadow: .1em .1em 0 hsl(39, 94%, 6%);
-                    font-family: 'Helvetica',serif;
-                    font-weight: 700;
-                    text-transform: uppercase;
-                    color: #f1a604; 
+                    width: 100%;
+                }
+
+                .split-text-container {
+                    font-size: 5rem;
+                    font-weight: 800;
+                    display: flex;
                     overflow: hidden;
-                    letter-spacing: 3px;
+                    align-items: center;
+                    justify-content: center;
                     text-align: center;
+                    letter-spacing: 2px;
+                    text-transform: uppercase;
+                    color: #FCA311;
+                    text-shadow: 2px 2px 0 #000000;
                 }
 
                 .text-part {
                     display: inline-block;
-                    position: relative;
-                    animation-duration: 1.8s;
-                    animation-timing-function: ease-out;
-                    animation-fill-mode: forwards;
+                    animation-duration: 1.2s;
+                    animation-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
                     opacity: 0;
+                    animation-fill-mode: forwards;
                 }
 
                 .text-part.left {
@@ -70,156 +69,215 @@ st.markdown("""
                 }
 
                 @keyframes slide-in-left {
-                    0% {
-                        transform: translateX(-200%);
-                        opacity: 0;
-                    }
-                    100% {
+                    to {
                         transform: translateX(0);
                         opacity: 1;
                     }
                 }
 
                 @keyframes slide-in-right {
-                    0% {
-                        transform: translateX(200%);
-                        opacity: 0;
-                    }
-                    100% {
+                    to {
                         transform: translateX(0);
                         opacity: 1;
                     }
                 }
 
                 /* Title styling */
-                
-                
                 .hero-title {
-                    font-size: 65px;
+                    font-size: 48px;
                     font-weight: 800;
-                    color: #111;
+                    text-align: center;
+                    color: #14213D;
                 }
 
                 .hero-subtitle {
-                    font-size: 40px;
-                    color: #777;
-                    margin-top: -20px;
+                    font-size: 32px;
+                    font-weight: 700;
+                    margin-bottom: 15px;
+                    color: #FCA311;
                 }
 
                 /* Paragraph */
                 .hero-text {
                     font-size: 18px;
-                    color: #444;
+                    font-style: italic;
                     line-height: 1.6;
-                    max-width: 500px;
+                    max-width: 90%;
+                    color: #14213D;
                 }
 
+                .step-card {
+                    display: flex;
+                    align-items: center;
+                    flex-direction: column;
+                    justify-content: center;
+                    width: 200px;
+                    height: 350px;
+                    padding: 30px 20px;
+                    transition: transform 0.5s ease-in-out;
+                    text-align: center;
+                    border: 2px solid #E5E5E5;
+                    border-radius: 12px;
+                    background-color: #FCA311;
+                    box-shadow: 0 10px 30px rgba(20, 33, 61, 0.15);
+                }
 
+                .step-card:hover {
+                    transform: translateY(-5px);
+                    border-color: #14213D;
+                    box-shadow: 0 20px 40px rgba(20, 33, 61, 0.25);
+                }
+
+                .step-icon {
+                    font-size: 40px;
+                    color: #14213D;
+                }
+
+                .step-title {
+                    font-size: 20px;
+                    font-weight: 700;
+                    margin-bottom: 20px;
+                    color: #14213D;
+                }
+
+                .step-sub {
+                    font-size: 15px;
+                    margin: 0;
+                    color: #14213D;
+                }
+
+                .arrow-container {
+                    font-size: 40px;
+                    font-weight: bolder;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    margin-top: 125px;
+                }
+
+                div.stButton > button {
+                    font-size: 28px;
+                    font-weight: 800;
+                    padding: 15px 30px;
+                    transition: all 0.3s ease;
+                    letter-spacing: 1px;
+                    text-transform: uppercase;
+                    color: #14213D;
+                    border: none;
+                    border-radius: 8px;
+                    background-color: #FCA311;
+                    box-shadow: 0 10px 30px rgba(252, 163, 17, 0.4);
+                }
+
+                div.stButton > button:hover {
+                    transform: translateY(-2px);
+                    color: #000000;
+                    background-color: #E5940F;
+                    box-shadow: 0 6px 20px rgba(252, 163, 17, 0.5);
+                }
+
+                div.stButton > button:focus {
+                    border: none;
+                    outline: none;
+                }
             </style>
             """, unsafe_allow_html=True)
 
-st.markdown("""
-<div class='hero-section'>
-    <div class="hero-title-wrapper">
-        <div class="split-text-container">
-            <div class="text-part left">WORD OF </div>
-            <div style="width: 28px;"></div>
-            <div class="text-part right">MARKETING</div>
+st.markdown(
+    """
+    <div class="hero-section">
+        <div class="hero-title-wrapper">
+            <div class="split-text-container">
+                <div class="text-part left">WORD OF</div>
+                <div style="width: 30px;"></div>
+                <div class="text-part right">MARKETING</div>
+            </div>
         </div>
     </div>
-</div>
-""", unsafe_allow_html=True)
+    """,
+    unsafe_allow_html=True
+)
 
-st.markdown("<div class='content-card'>", unsafe_allow_html=True)
+st.markdown('<div class="hero-title">Create Fast, Save Time</div>', unsafe_allow_html=True)
+st.markdown('<div style="height: 40px;"/>', unsafe_allow_html=True)
 
-col1, col2 = st.columns([1, 1])
+col1, col2 = st.columns([1, 1], gap='large')
 
 with col1:
-    st.markdown("<div class='hero-title'>Create Fast, Save Time</div>", unsafe_allow_html=True)
-    st.markdown("<div class='hero-subtitle'>From concept to campaign in minutes</div>", unsafe_allow_html=True)
+    st.markdown('<div class="hero-subtitle">From concept to campaign in minutes</div>', unsafe_allow_html=True)
 
-    st.markdown("""
-    <div class='hero-text'>
-        Save hours of designing and formatting. Our marketing tool generates ready to use, platform-specific creatives in just a few clicks.
-    </div>
-    """, unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="hero-text">
+            Save hours of designing and formatting. Our marketing tool generates ready to use, platform-specific
+            creatives in just a few clicks.
+        </div>""",
+        unsafe_allow_html=True
+    )
 
 with col2:
-    st.markdown("<div class='hero-title'>Create Fast, Save Time</div>", unsafe_allow_html=True)
-    st.markdown("<div class='hero-subtitle'>Guidelines and Creativity adhered</div>", unsafe_allow_html=True)
+    st.markdown('<div class="hero-subtitle">Guidelines and Creativity adhered</div>', unsafe_allow_html=True)
 
-    st.markdown("""
-        <div class='hero-text'>
-            Generate marketing posters that adhere to both retailer and brand guidelines automatically. Customize freely while staying within safe, professional boundaries ensuring your campaigns always look polished.
-        </div>
-        """, unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="hero-text">
+            Generate marketing posters that adhere to both retailer and brand guidelines automatically. Customize freely
+            while staying within safe, professional boundaries ensuring your campaigns always look polished.
+        </div>""",
+        unsafe_allow_html=True
+    )
 
-st.markdown("<h2 style='text-align:center;'>How Our Tool Works</h2>", unsafe_allow_html=True)
+st.divider()
 
-card_css = """
-           <style>
-               .step-card {
-                   background-color: #f5af28;
-                   padding: 20px;
-                   border-radius: 15px;
-                   text-align: center;
-                   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
-
-
-                   height: 350px;
-                   display: flex;
-                   flex-direction: column;
-                   justify-content: center; /* vertical center */
-                   align-items: center; /* horizontal center */
-               }
-
-               .step-title {
-                   margin: 10px 0 5px 0;
-                   font-size: 18px;
-                   font-weight: 600;
-               }
-
-               .step-sub {
-                   margin: 0;
-                   font-size: 14px;
-                   color: #555;
-               }
-           </style> \
-           """
-st.markdown(card_css, unsafe_allow_html=True)
+st.markdown('<h2 style="text-align:center; font-weight: 800;">How Our Tool Works</h2>', unsafe_allow_html=True)
+st.markdown('<div style="height: 40px;"/>', unsafe_allow_html=True)
 
 steps = [
-    {"title": "Input Campaign Details",
-     "subtitle": "Tell us your product and goals. Drop Packshots of Logo and Product", "icon": "❖"},
-    {"title": "Choose Design Preference", "subtitle": "Fonts, Background Colour, Layout ", "icon": "❖"},
-    {"title": "Customize And Refactor", "subtitle": "Review and Reformat according to your liking.", "icon": "❖"},
-    {"title": "Export Your Poster", "subtitle": "Download your platform-ready creatives", "icon": "❖"}
-]
+    {
+        'title': 'Input Campaign Details',
+        'subtitle': 'Tell us your product and goals. Drop Packshots of Logo and Product',
+        'icon': '❖'
+    },
 
-arrow_img_path = "icons/arrow-right-solid-full.svg"
+    {
+        'title': 'Choose Design Preference',
+        'subtitle': 'Fonts, Background Colour, Layout ',
+        'icon': '❖'
+    },
+
+    {
+        'title': 'Customize And Refactor',
+        'subtitle': 'Review and Reformat according to your liking.',
+        'icon': '❖'
+    },
+
+    {
+        'title': 'Export Your Poster',
+        'subtitle': 'Download your platform-ready creatives',
+        'icon': '❖'
+    }
+]
 
 cols = st.columns(7)
 for i in range(len(steps)):
-
     with cols[i * 2]:
         st.markdown(
-            f"""
-               <div class="step-card">
-                   <div style="width:60px; margin-bottom:12px;">{steps[i]['icon']}</div>
-                   <div class="step-title">{steps[i]['title']}</div>
-                   <p class="step-sub">{steps[i]['subtitle']}</p>
-               </div>
-               """,
+            f"""<div class="step-card">
+    <div class="step-icon">{steps[i]['icon']}</div>
+    <div class="step-title">{steps[i]['title']}</div>
+    <p class="step-sub">{steps[i]['subtitle']}</p>
+</div>""",
             unsafe_allow_html=True,
         )
 
     if i < len(steps) - 1:
         with cols[i * 2 + 1]:
-            st.markdown("<br><br><br>", unsafe_allow_html=True)
-            st.image(arrow_img_path, width=90)
+            st.markdown('<div class="arrow-container">→</div>', unsafe_allow_html=True)
 
 st.divider()
+
 _, mid, _ = st.columns([1, 2, 1])
 with mid:
-    if st.button("Start Designing", use_container_width=True):
-        st.switch_page("pages/Assets_Page.py")
+    st.markdown('<div style="height: 20px;"</div>', unsafe_allow_html=True)
+    if st.button('Start Designing', use_container_width=True):
+        st.switch_page('pages/Assets_Page.py')
