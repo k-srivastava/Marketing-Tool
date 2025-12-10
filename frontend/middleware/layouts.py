80f7f10da22a6aef94a65bbe1500d3a5a34ddebd
k-srivastava <srivastavak1223@gmail.com>
2025-12-10 15:20:14 +0530

    Created page heading population.

--- frontend/middleware/layouts.py
+++ frontend/middleware/layouts.py
@@ -19,3 +19,21 @@ def generate_page_labels(num_support_images: int) -> list[str]:
         page_labels.append(f'Support Image ({i + 1}) Placement')
 
     return page_labels
+
+
+def generate_page_headings(num_support_images: int) -> list[str]:
+    """
+    Generate headings for the layout preference page based on the number of support images.
+
+    :param num_support_images: Number of support images to include in the layout.
+    :type num_support_images: int
+
+    :return: List of page headings.
+    :rtype: list[str]
+    """
+    page_headings = ['Hero Layout', 'Logo Layout']
+
+    for i in range(num_support_images):
+        page_headings.append(f'Support Image ({i + 1}) Layout')
+
+    return page_headings
