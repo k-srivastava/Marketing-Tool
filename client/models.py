433970741f097850c39b7f5f3f93beb921badbf2
k-srivastava <srivastavak1223@gmail.com>
2025-12-12 14:24:20 +0530

    Added a font recommendation model.

--- client/models.py
+++ client/models.py
@@ -24,3 +24,33 @@ class ProductInformation(BaseModel):
     tagline: str
     brand_name: str
     features: list[str]
+
+
+class FontRecommendation(BaseModel):
+    """
+    Represents font names and HTML links to load fonts into CSS>
+
+    :ivar font_1: First font name.
+    :type font_1: str
+    :ivar font_1_link: HTML link tag for the first font.
+    :ivar font_2: Second font name.
+    :type font_2: str
+    :ivar font_2_link: HTML link tag for the second font.
+    :type font_2_link: str
+    :ivar font_3: Third font name.
+    :type font_3: str
+    :ivar font_3_link: HTML link tag for the third font.
+    :type font_3_link: str
+    :ivar font_4: Fourth font name.
+    :type font_4: str
+    :ivar font_4_link: HTML link tag for the fourth font.
+    :type font_4_link: str
+    """
+    font_1: str
+    font_1_link: str
+    font_2: str
+    font_2_link: str
+    font_3: str
+    font_3_link: str
+    font_4: str
+    font_4_link: str
